1f9994a5e368c4a1b61dfb7da38a53bd4aa4b9e2
jimipasa <sumit.shrestha@gmail.com>
2015-10-01 17:25:45 -0400

    initial commit

--- StockTicker.py
+++ StockTicker.py
@@ -3,7 +3,6 @@ from googlefinance import getQuotes
 import json
 tickers = {'AAPL':'Apple ','NFLX':'Netflix ', 'V' :'Visa ', 'LUV':'SouthWest '}
 for tick in tickers.keys():
-    result = json.dumps(getQuotes(tick), indent=2)
     LTPrice = json.loads(json.dumps(getQuotes(tick), indent=2))[0]['LastTradePrice']
     LTTime = json.loads(json.dumps(getQuotes(tick), indent=2))[0]['LastTradeDateTimeLong']
     print ('Last traded price for ' + tickers[tick] + 'on '+ LTTime +' is ' + LTPrice + ' dollars')
